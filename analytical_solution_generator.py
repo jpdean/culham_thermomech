61992913d7acbb776d697e55e4f66c39a54bf76f
Joe Dean <jpd62@cam.ac.uk>
2022-01-13 17:36:11 +0000

    Add and test Neumann bc

--- analytical_solution_generator.py
+++ analytical_solution_generator.py
@@ -2,6 +2,24 @@ from sympy import symbols
 from sympy.vector import CoordSys3D, gradient, divergence
 from sympy import sin, cos, pi, simplify
 
+
+def sympy_to_code(sympy_func):
+    replacement_strings = (("pi", "np.pi"),
+                           ("sin", "np.sin"),
+                           ("cos", "np.cos"),
+                           ("R.x", "x[0]"),
+                           ("R.y", "x[1]"),
+                           ("*", " * "))
+    code = str(sympy_func)
+    for s in replacement_strings:
+        code = code.replace(s[0], s[1])
+
+    # HACK To fix replaced power spacing.
+    # FIXME Implement function properly with regex
+    code = code.replace(" *  * ", "**")
+    return code
+
+
 R = CoordSys3D("R")
 t = symbols("t")
 
@@ -12,15 +30,10 @@ rho = 2.7 + T**2
 
 f = simplify(rho * c * T.diff(t) - divergence(kappa * gradient(T)))
 
-# FIXME This currently adds undesired spacing to powers
-replacement_strings = (("pi", "np.pi"),
-                       ("sin", "np.sin"),
-                       ("cos", "np.cos"),
-                       ("R.x", "x[0]"),
-                       ("R.y", "x[1]"),
-                       ("*", " * "))
-f_code = str(f)
-for s in replacement_strings:
-    f_code = f_code.replace(s[0], s[1])
+f_code = sympy_to_code(f)
+print(f"f = {f_code}")
 
-print(f_code)
+# Evaluate Neumann BC on right face
+dT_dn_right = simplify((kappa * gradient(T)).dot(R.i))
+dT_dn_right_code = sympy_to_code(dT_dn_right)
+print(f"\ndT_dn_right = {dT_dn_right_code}")
--- transient_heat.py
+++ transient_heat.py
@@ -27,8 +27,7 @@ def solve(mesh, k, t_end, num_time_steps, problem):
 
     facet_dim = mesh.topology.dim - 1
     boundary_facets = locate_entities_boundary(
-        mesh, facet_dim, lambda x: np.full(
-            x.shape[1], True, dtype=bool))
+        mesh, facet_dim, problem.dirichlet_boundary_marker)
     T_d = fem.Function(V)
     T_d.interpolate(problem.T)
     bc = fem.dirichletbc(
@@ -53,12 +52,17 @@ def solve(mesh, k, t_end, num_time_steps, problem):
     f = fem.Function(V)
     f.interpolate(problem.f)
 
+    # FIXME Simple constant may be sufficient instead of function for Neumann BC
+    kappa_dT_dn = fem.Function(V)
+    kappa_dT_dn.interpolate(problem.neumann_bc)
+
     rho = problem.rho(T_h)
     c = problem.c(T_h)
     kappa = problem.kappa(T_h)
     F = ufl.inner(rho * c * T_h, v) * ufl.dx + \
         delta_t * ufl.inner(kappa * ufl.grad(T_h), ufl.grad(v)) * ufl.dx - \
-        ufl.inner(rho * c * T_n + delta_t * f, v) * ufl.dx
+        ufl.inner(rho * c * T_n + delta_t * f, v) * ufl.dx - \
+        delta_t * ufl.inner(kappa_dT_dn, v) * ufl.ds
 
     non_lin_problem = fem.NonlinearProblem(F, T_h, [bc])
     solver = NewtonSolver(MPI.COMM_WORLD, non_lin_problem)
@@ -75,6 +79,7 @@ def solve(mesh, k, t_end, num_time_steps, problem):
 
         T_d.interpolate(problem.T)
         f.interpolate(problem.f)
+        kappa_dT_dn.interpolate(problem.neumann_bc)
 
         n, converged = solver.solve(T_h)
         T_h.x.scatter_forward()
@@ -132,6 +137,18 @@ class Problem():
         x = np.array([0.0, 0.25, 0.50, 0.75, 1.0])
         y = np.array([4.1, 4.1625, 4.35, 4.6625, 5.1])
         return ufl_poly_from_table_data(x, y, 2, T)
+
+    def dirichlet_boundary_marker(self, x):
+        # TODO Replace with meshtags etc.
+        return np.logical_or(np.isclose(x[0], 0.0),
+                             np.logical_or(np.isclose(x[1], 0.0),
+                                           np.isclose(x[1], 1.0)))
+
+    def neumann_bc(self, x):
+        # TODO Implement proper BC interface
+        # NOTE This is just the Neumann BC for the right boundary
+        # TODO Implment with UFL instead?
+        return np.pi * (np.sin(x[0] * np.pi)**2 * np.sin(np.pi * self.t)**2 * np.cos(x[1] * np.pi)**2 + 4.1) * np.sin(np.pi * self.t) * np.cos(x[0] * np.pi) * np.cos(x[1] * np.pi)
 
 
 def main():

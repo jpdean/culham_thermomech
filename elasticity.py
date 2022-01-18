0b4431fbe52024fefe449362abccf1184fe1ab91
Joe Dean <jpd62@cam.ac.uk>
2022-01-18 16:17:29 +0000

    Imports

--- elasticity.py
+++ elasticity.py
@@ -4,8 +4,7 @@ from dolfinx.fem import (VectorFunctionSpace, dirichletbc,
                          locate_dofs_geometrical, LinearProblem)
 from dolfinx.io import XDMFFile
 from dolfinx.mesh import create_unit_square
-from ufl import (Identity, TestFunction, TrialFunction, as_vector,
-                 dx, grad, inner, sym, tr)
+import ufl
 
 from mpi4py import MPI
 from petsc4py import PETSc
@@ -25,19 +24,19 @@ lmbda = E * nu / ((1.0 + nu) * (1.0 - 2.0 * nu))
 
 def sigma(v):
     # Elastic strain
-    eps_e = sym(grad(v))
+    eps_e = ufl.sym(ufl.grad(v))
     eps = eps_e  # TODO Add thermal strain
-    return 2.0 * mu * eps + lmbda * tr(eps) * Identity(
+    return 2.0 * mu * eps + lmbda * ufl.tr(eps) * ufl.Identity(
         len(v))
 
 
 V = VectorFunctionSpace(mesh, ("Lagrange", 1))
 
-u = TrialFunction(V)
-v = TestFunction(V)
-f = as_vector((0.0, - 1.0))
-a = inner(sigma(u), grad(v)) * dx
-L = inner(f, v) * dx  # TODO Add pressure BC
+u = ufl.TrialFunction(V)
+v = ufl.TestFunction(V)
+f = ufl.as_vector((0.0, - 1.0))
+a = ufl.inner(sigma(u), ufl.grad(v)) * ufl.dx
+L = ufl.inner(f, v) * ufl.dx  # TODO Add pressure BC
 
 bc = dirichletbc(np.array([0, 0, 0], dtype=PETSc.ScalarType),
                  locate_dofs_geometrical(V, lambda x: np.isclose(x[0], 0.0)),
@@ -50,3 +49,5 @@ u_h = problem.solve()
 with XDMFFile(MPI.COMM_WORLD, "u.xdmf", "w") as file:
     file.write_mesh(mesh)
     file.write_function(u_h)
+
+# u_e = ufl.as_

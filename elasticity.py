e9314344c97acd87daa37b39d9a5fdc7109742bd
Joe Dean <jpd62@cam.ac.uk>
2022-01-19 11:00:26 +0000

    Add solve function

--- elasticity.py
+++ elasticity.py
@@ -11,66 +11,74 @@ from mpi4py import MPI
 from petsc4py import PETSc
 
 
-n = 32
-mesh = create_unit_square(MPI.COMM_WORLD, n, n)
+def solve(mesh, k, nu, T, T_ref, E, alpha_L, u_e):
+    def sigma(v, T, T_ref, alpha_L):
+        # Elastic strain
+        eps_e = ufl.sym(ufl.grad(v))
+        eps_T = alpha_L * (T - T_ref) * ufl.Identity(len(v))
+        eps = eps_e - eps_T
+        mu = E / (2.0 * (1.0 + nu))
+        lmbda = E * nu / ((1.0 + nu) * (1.0 - 2.0 * nu))
+        return 2.0 * mu * eps + lmbda * ufl.tr(eps) * ufl.Identity(len(v))
 
-# Poisson's ratio
-nu = 0.33
-# Thermal expansion coefficient
-# FIXME Do this properly
-T_ref = 1.5
-x = ufl.SpatialCoordinate(mesh)
-T = T_ref + ufl.cos(ufl.pi * x[0]) * ufl.sin(ufl.pi * x[1])
-# Young's modulus
-E = 1.0 + 0.1 * T**2
-alpha_L = 0.1 + 0.01 * T**3
+    f = - ufl.div(sigma(u_e, T, T_ref, alpha_L))
+    p_n = ufl.dot(sigma(u_e, T, T_ref, alpha_L), ufl.FacetNormal(mesh))
 
+    V = VectorFunctionSpace(mesh, ("Lagrange", k))
 
-def sigma(v, T, T_ref, alpha_L):
-    # Elastic strain
-    eps_e = ufl.sym(ufl.grad(v))
-    eps_T = alpha_L * (T - T_ref) * ufl.Identity(len(v))
-    eps = eps_e - eps_T
-    mu = E / (2.0 * (1.0 + nu))
-    lmbda = E * nu / ((1.0 + nu) * (1.0 - 2.0 * nu))
-    return 2.0 * mu * eps + lmbda * ufl.tr(eps) * ufl.Identity(len(v))
+    u = ufl.TrialFunction(V)
+    v = ufl.TestFunction(V)
+    F = ufl.inner(sigma(u, T, T_ref, alpha_L), ufl.grad(v)) * ufl.dx - \
+        ufl.inner(f, v) * ufl.dx - ufl.inner(p_n, v) * ufl.ds
+    a = ufl.lhs(F)
+    L = ufl.rhs(F)
 
+    def dirichlet_boundary(x):
+        return np.logical_or(np.logical_or(np.isclose(x[0], 0.0),
+                                           np.isclose(x[0], 1.0)),
+                             np.isclose(x[1], 0.0))
 
-u_e = ufl.as_vector((ufl.sin(ufl.pi * x[0]) * ufl.sin(ufl.pi * x[1]),
-                     ufl.sin(ufl.pi * x[0]) * ufl.sin(ufl.pi * x[1])))
-f = - ufl.div(sigma(u_e, T, T_ref, alpha_L))
-p_n = ufl.dot(sigma(u_e, T, T_ref, alpha_L), ufl.FacetNormal(mesh))
+    bc = dirichletbc(np.array([0, 0, 0], dtype=PETSc.ScalarType),
+                     locate_dofs_geometrical(V, dirichlet_boundary),
+                     V)
 
-V = VectorFunctionSpace(mesh, ("Lagrange", 1))
+    problem = LinearProblem(a, L, bcs=[bc],
+                            petsc_options={"ksp_type": "preonly",
+                                           "pc_type": "lu"})
+    u_h = problem.solve()
+    u_h.name = "u"
 
-u = ufl.TrialFunction(V)
-v = ufl.TestFunction(V)
-F = ufl.inner(sigma(u, T, T_ref, alpha_L), ufl.grad(v)) * ufl.dx - \
-    ufl.inner(f, v) * ufl.dx - ufl.inner(p_n, v) * ufl.ds
-a = ufl.lhs(F)
-L = ufl.rhs(F)
+    with XDMFFile(MPI.COMM_WORLD, "u.xdmf", "w") as file:
+        file.write_mesh(mesh)
+        file.write_function(u_h)
 
-
-def dirichlet_boundary(x):
-    return np.logical_or(np.logical_or(np.isclose(x[0], 0.0),
-                                       np.isclose(x[0], 1.0)),
-                         np.isclose(x[1], 0.0))
+    return u_h
 
 
-bc = dirichletbc(np.array([0, 0, 0], dtype=PETSc.ScalarType),
-                 locate_dofs_geometrical(V, dirichlet_boundary),
-                 V)
+def main():
+    n = 32
+    k = 1
+    mesh = create_unit_square(MPI.COMM_WORLD, n, n)
+    # Poisson's ratio
+    nu = 0.33
+    # Thermal expansion coefficient
+    # FIXME Do this properly
+    T_ref = 1.5
+    x = ufl.SpatialCoordinate(mesh)
+    T = T_ref + ufl.cos(ufl.pi * x[0]) * ufl.sin(ufl.pi * x[1])
+    # Young's modulus
+    E = 1.0 + 0.1 * T**2
+    alpha_L = 0.1 + 0.01 * T**3
+    u_e = ufl.as_vector((ufl.sin(ufl.pi * x[0]) * ufl.sin(ufl.pi * x[1]),
+                         ufl.sin(ufl.pi * x[0]) * ufl.sin(ufl.pi * x[1])))
 
-problem = LinearProblem(a, L, bcs=[bc], petsc_options={"ksp_type": "preonly",
-                                                       "pc_type": "lu"})
-u_h = problem.solve()
-u_h.name = "u"
+    u_h = solve(mesh, k, nu, T, T_ref, E, alpha_L, u_e)
 
-with XDMFFile(MPI.COMM_WORLD, "u.xdmf", "w") as file:
-    file.write_mesh(mesh)
-    file.write_function(u_h)
+    error_L2 = np.sqrt(mesh.comm.allreduce(assemble_scalar(
+        form((u_h - u_e)**2 * ufl.dx)), op=MPI.SUM))
+    if mesh.comm.Get_rank() == 0:
+        print(error_L2)
 
-error_L2 = np.sqrt(mesh.comm.allreduce(assemble_scalar(
-    form((u_h - u_e)**2 * ufl.dx)), op=MPI.SUM))
-if mesh.comm.Get_rank() == 0:
-    print(error_L2)
+
+if __name__ == "__main__":
+    main()

99d8aa6f9a1374eaedf937d77c26f32046b7ce6e
Joe Dean <jpd62@cam.ac.uk>
2022-01-19 12:34:28 +0000

    Refactor

--- elasticity.py
+++ elasticity.py
@@ -11,25 +11,24 @@ from mpi4py import MPI
 from petsc4py import PETSc
 
 
-def solve(mesh, k, nu, T, T_ref, E, alpha_L, u_e):
-    def sigma(v, T, T_ref, alpha_L):
-        # Elastic strain
-        eps_e = ufl.sym(ufl.grad(v))
-        eps_T = alpha_L * (T - T_ref) * ufl.Identity(len(v))
-        eps = eps_e - eps_T
-        mu = E / (2.0 * (1.0 + nu))
-        lmbda = E * nu / ((1.0 + nu) * (1.0 - 2.0 * nu))
-        return 2.0 * mu * eps + lmbda * ufl.tr(eps) * ufl.Identity(len(v))
+def sigma(v, T, T_ref, alpha_L, E, nu):
+    # Elastic strain
+    eps_e = ufl.sym(ufl.grad(v))
+    eps_T = alpha_L * (T - T_ref) * ufl.Identity(len(v))
+    eps = eps_e - eps_T
+    mu = E / (2.0 * (1.0 + nu))
+    lmbda = E * nu / ((1.0 + nu) * (1.0 - 2.0 * nu))
+    return 2.0 * mu * eps + lmbda * ufl.tr(eps) * ufl.Identity(len(v))
 
-    f = - ufl.div(sigma(u_e, T, T_ref, alpha_L))
-    p_n = ufl.dot(sigma(u_e, T, T_ref, alpha_L), ufl.FacetNormal(mesh))
 
+def solve(mesh, k, nu, T, T_ref, E, alpha_L, f, p):
     V = VectorFunctionSpace(mesh, ("Lagrange", k))
 
     u = ufl.TrialFunction(V)
     v = ufl.TestFunction(V)
-    F = ufl.inner(sigma(u, T, T_ref, alpha_L), ufl.grad(v)) * ufl.dx - \
-        ufl.inner(f, v) * ufl.dx - ufl.inner(p_n, v) * ufl.ds
+    F = ufl.inner(sigma(u, T, T_ref, alpha_L, E, nu), ufl.grad(v)) * ufl.dx - \
+        ufl.inner(f, v) * ufl.dx - \
+        ufl.inner(ufl.dot(p, ufl.FacetNormal(mesh)), v) * ufl.ds
     a = ufl.lhs(F)
     L = ufl.rhs(F)
 
@@ -71,8 +70,14 @@ def main():
     alpha_L = 0.1 + 0.01 * T**3
     u_e = ufl.as_vector((ufl.sin(ufl.pi * x[0]) * ufl.sin(ufl.pi * x[1]),
                          ufl.sin(ufl.pi * x[0]) * ufl.sin(ufl.pi * x[1])))
+    f = - ufl.div(sigma(u_e, T, T_ref, alpha_L, E, nu))
+    # Here, purely for simplicity and the purposes of comparing with the
+    # analytical solution, `p` is a tensor not a scalar, since
+    # \sigma \cdot n = p n. Could also find a scalar, but there isn't much
+    # point in this case.
+    p = sigma(u_e, T, T_ref, alpha_L, E, nu)
 
-    u_h = solve(mesh, k, nu, T, T_ref, E, alpha_L, u_e)
+    u_h = solve(mesh, k, nu, T, T_ref, E, alpha_L, f, p)
 
     error_L2 = np.sqrt(mesh.comm.allreduce(assemble_scalar(
         form((u_h - u_e)**2 * ufl.dx)), op=MPI.SUM))

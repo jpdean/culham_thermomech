20d0085017cfb4c971603b2e6267f94c45a85d0f
Joe Dean <jpd62@cam.ac.uk>
2022-01-25 14:48:42 +0000

    Add Inconel 625

--- materials.py
+++ materials.py
@@ -6,7 +6,6 @@ materials = {}
 
 
 # Copper
-# Thermal Conductivity
 materials["copper"] = \
     {"kappa":
      lambda T: ufl_poly_from_table_data(
@@ -19,7 +18,7 @@ materials["copper"] = \
              [401.0, 398.0, 395.0, 391.0, 388.0, 384.0, 381.0, 378.0,
               374.0, 371.0, 367.0, 364.0, 360.0, 357.0, 354.0, 350.0,
               347.0, 344.0, 340.0, 337.0, 334.0]),
-         degree=1, u=T, num_pieces=1),
+         u=T, degree=1, num_pieces=1),
      "c":
      lambda T: ufl_poly_from_table_data(
          x=np.array(
@@ -31,7 +30,7 @@ materials["copper"] = \
              [388.0, 390.0, 394.0, 398.0, 401.0, 406.0, 410.0, 415.0,
               419.0, 424.0, 430.0, 435.0, 441.0, 447.0, 453.0, 459.0,
               466.0, 472.0, 479.0, 487.0, 494.0]),
-         degree=2, u=T, num_pieces=1),
+         u=T, degree=2, num_pieces=1),
      "thermal_strain":
      (lambda T: ufl_poly_from_table_data(
          x=np.array(
@@ -42,7 +41,7 @@ materials["copper"] = \
              [16.7, 17.0, 17.2, 17.5, 17.7, 17.8, 18.0, 18.1, 18.2,
               18.4, 18.5, 18.65, 18.8, 18.97, 19.14, 19.34, 19.55,
               19.78, 20.05]) * 1e-6,
-         degree=3, u=T, num_pieces=1), 293.15),
+         u=T, degree=3, num_pieces=1), 293.15),
      "rho":
      lambda T: ufl_poly_from_table_data(
          x=np.array(
@@ -53,7 +52,7 @@ materials["copper"] = \
              [8940.0, 8926.0, 8903.0, 8879.0, 8854.0, 8829.0, 8802.0,
               8774.0, 8744.0, 8713.0, 8681.0, 8647.0, 8612.0, 8575.0,
               8536.0, 8495.0, 8453.0, 8409.0, 8363.0]),
-         degree=2, u=T, num_pieces=1),
+         u=T, degree=2, num_pieces=1),
      "E":
      lambda T: ufl_poly_from_table_data(
          x=np.array(
@@ -62,6 +61,36 @@ materials["copper"] = \
          y=np.array(
              [117.0, 116.0, 114.0, 112.0, 110.0, 108.0, 105.0, 102.0,
               98.0]) * 1e9,
-         degree=2, u=T, num_pieces=1),
+         u=T, degree=2, num_pieces=1),
      "nu": 0.33
      }
+
+# Inconel 625
+materials["inconel_625"] = \
+    {"kappa": lambda T: ufl_poly_from_table_data(
+        x=np.array(
+            [293.15, 373.15, 423.15, 473.15, 523.15, 573.15, 623.15, 673.15]),
+        y=np.array(
+            [9.8, 10.9, 11.7, 12.4, 13.2, 13.9, 14.7, 15.4]),
+        u=T, degree=1, num_pieces=1),
+     "c": lambda T: ufl_poly_from_table_data(
+        x=np.array(
+            [293.15, 373.15, 473.15, 573.15, 673.15]),
+        y=np.array(
+            [407.0, 419.0, 447.0, 475.0, 499.0]),
+        u=T, degree=3, num_pieces=1),
+     "E": lambda T: ufl_poly_from_table_data(
+        x=np.array(
+            [293.15, 373.15, 423.15, 473.15, 523.15, 573.15, 623.15, 673.15]),
+        y=np.array(
+            [207.0, 202.0, 199.0, 197.0, 194.0, 191.0, 189.0, 186.0]) * 1e9,
+        u=T, degree=1, num_pieces=1),
+     "thermal_strain": (lambda T: ufl_poly_from_table_data(
+         x=np.array(
+             [293.15, 323.15, 373.15, 423.15, 473.15, 523.15, 573.15,
+              623.15, 673.15, 723.15, 773.15, 823.15, 873.15, 923.15,
+              973.15, 1023.15, 1073.15, 1123.15, 1173.15]),
+         y=np.array(
+             [16.7, 17.0, 17.2, 17.5, 17.7, 17.8, 18.0, 18.1, 18.2, 18.4,
+              18.5, 18.65, 18.8, 18.97, 19.14, 19.34, 19.55, 19.78,
+              20.05]) * 1e-6), 293.15)}

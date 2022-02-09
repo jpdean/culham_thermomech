faa41c212d5176291c0f4ff48d88c42f954df204
Joseph P. Dean <jpd62@cam.ac.uk>
2022-02-09 14:18:40 +0000

    Write to JSON

--- csut.py
+++ csut.py
@@ -5,6 +5,7 @@ import numpy as np
 from petsc4py import PETSc
 from dolfinx import fem
 from thermomech import solve
+import json
 
 with XDMFFile(MPI.COMM_WORLD, "csut.xdmf", "r") as f:
     mesh = f.read_mesh()
@@ -14,9 +15,10 @@ with XDMFFile(MPI.COMM_WORLD, "csut.xdmf", "r") as f:
     bc_mt["u"] = f.read_meshtags(mesh, "boundaries_u")
     material_mt = f.read_meshtags(mesh, "materials")
 
-t_end = 20
+delta_t = 5
+num_time_steps = 10
+t_end = num_time_steps * delta_t
 k = 1
-num_time_steps = 4
 
 materials = []
 materials.append(mat_dict["Copper"])
@@ -54,6 +56,12 @@ def T_i(x): return 293.15 * np.ones_like(x[0])
 
 g = PETSc.ScalarType(- 9.81)
 
-solve(mesh, k, t_end, num_time_steps, T_i, f_T,
-      f_u, g, materials, material_mt, bcs, bc_mt,
-      write_to_file=True)
+results = solve(mesh, k, t_end, num_time_steps, T_i, f_T,
+                f_u, g, materials, material_mt, bcs, bc_mt,
+                write_to_file=True, steps_per_write=1)
+
+n_procs = MPI.COMM_WORLD.Get_size()
+
+if mesh.comm.Get_rank() == 0:
+    with open(f"csut_{n_procs}.json", "w") as f:
+        json.dump(results["data"], f)

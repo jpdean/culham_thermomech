a2f5d10e11213c9d52b09952b768888ca4bf922d
Joseph P. Dean <jpd62@cam.ac.uk>
2022-02-09 12:34:41 +0000

    Add ufl linear interpolation

--- utils.py
+++ utils.py
@@ -7,6 +7,26 @@ import ufl
 from mpi4py import MPI
 
 
+def ufl_linear_interp(xs, ys, u):
+    ms = []
+    cs = []
+    for i in range(len(xs) - 1):
+        ms.append((ys[i + 1] - ys[i]) / (xs[i + 1] - xs[i]))
+        cs.append(ys[i] - ms[i] * xs[i])
+
+    conditions = [ufl.gt(u, x) for x in xs]
+    pieces = [m * u + c for (m, c) in zip(ms, cs)]
+    # If u < xs[-1], extrapolate using the last piece
+    pieces.append(pieces[-1])
+
+    interp = ufl.conditional(conditions[1], pieces[1], pieces[0])
+    for i in range(1, len(conditions) - 1):
+        interp = ufl.conditional(conditions[i + 1],
+                                 pieces[i + 1],
+                                 interp)
+    return interp
+
+
 def ufl_poly_from_table_data(x, y, u, degree, num_pieces=1):
     """Given a list of point data x and y, this function returns a fitted
     polynomial of degree `degree` in terms of the UFL `Function` `u`"""
